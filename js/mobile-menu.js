a304a7be07a58f070d1df750cdea185da5ee9569
KateOvchinikova <e23111999@gmail.com>
2022-11-01 21:16:49 +0200

    about-us

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,11 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-
-  menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
+$(document).ready(function () {
+  $("#mobile-menu-button").click(function () {
+    $("#mobile-nav").css("display", "block");
+    $("#body").css("overflow", "hidden");
   });
-})();
+
+  $("#mobile-nav-close").click(function () {
+    $("#mobile-nav").css("display", "none");
+    $("#body").css("overflow", "scroll");
+  });
+});
